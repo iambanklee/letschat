f84cf34003760d7af28b499e5fb7d5a894f0ecd4
Bank Lee <iambanklee@gmail.com>
2024-10-14 23:34:14 +0100

    broadcast when new chat room get created

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,3 +1,5 @@
 class ChatRoom < ApplicationRecord
   has_many :messages
+
+  after_create_commit { broadcast_append_to "chat_room_", target: "other_chat_rooms", partial: "chat_rooms/other_chat_room" }
 end
