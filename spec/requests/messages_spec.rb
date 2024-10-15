7db3e3fb64c54fe89f5f28aceba949645f596134
Bank Lee <iambanklee@gmail.com>
2024-10-15 20:44:33 +0100

    fix main#index test

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,18 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "Messages", type: :request do
-  describe "GET /index" do
+  let(:user) { create(:user) }
+
+  before { sign_in user }
+
+  describe "GET /chat_rooms/:chat_room_id/messages" do
+    subject(:request) do
+      get "/chat_rooms/#{chat_room.id}/messages", as: :turbo_stream
+    end
+
+    let(:chat_room) { create(:chat_room) }
+
     it "returns http success" do
-      get "/messages/index"
+      request
+
       expect(response).to have_http_status(:success)
+      expect(response.body).to include(/<turbo-stream action="update" target="chat_room_messages_area">/)
     end
   end
 
-  describe "GET /create" do
+  describe "POST /chat_rooms/:chat_room_id/messages" do
+    subject(:request) do
+      post "/chat_rooms/#{chat_room.id}/messages", params: message_params, as: :turbo_stream
+    end
+
+    let(:chat_room) { create(:chat_room) }
+    let(:message_params) do
+      { message: { content: "new message" } }
+    end
+
     it "returns http success" do
-      get "/messages/create"
+      request
+
       expect(response).to have_http_status(:success)
+      expect(response.body).to include(/<turbo-stream action="remove" target="does-not-exist">/)
+    end
+
+    it "creates a new message for given chat room" do
+      expect { request }.to change { chat_room.reload.messages.size }.by(1)
+
+      message = Message.last
+
+      expect(message.chat_room).to eq(chat_room)
+      expect(message.user).to eq(user)
+      expect(message.content).to eq("new message")
     end
   end
-
 end
--- spec/views/main/index.html.erb_spec.rb
+++ spec/views/main/index.html.erb_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe "main/index.html.erb", type: :view do
   let!(:user) { create(:user) }
   let!(:chat_rooms) { create_list(:chat_room, 2) }
+  let!(:other_chat_rooms) { create_list(:chat_room, 2) }
 
   before do
     assign(:current_user, user)
     assign(:my_chat_rooms, chat_rooms)
+    assign(:other_chat_rooms, other_chat_rooms)
   end
 
   it "display user display name" do
@@ -15,11 +17,15 @@ RSpec.describe "main/index.html.erb", type: :view do
     expect(rendered).to include(/#{user.display_name}/)
   end
 
-  it "display user's joined chat rooms" do
+  it "display chat rooms user joined" do
     render
 
-    expect(rendered).to match(/My chat rooms/)
-    expect(rendered).to match(/#{chat_rooms.first.name}/)
-    expect(rendered).to match(/#{chat_rooms.last.name}/)
+    expect(rendered).to match(/My chat rooms.*#{chat_rooms.first.name}.*#{chat_rooms.last.name}/m)
+  end
+
+  it "display other chat rooms user can join" do
+    render
+
+    expect(rendered).to match(/Other chat rooms.*#{other_chat_rooms.first.name}.*#{other_chat_rooms.last.name}/m)
   end
 end
