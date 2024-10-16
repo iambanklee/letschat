4b8aa62a76885be12c07c8e31240b703df95d9f6
Bank Lee <iambanklee@gmail.com>
2024-10-16 13:16:53 +0100

    align current user sent message to the right

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -2,6 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="messages"
 export default class extends Controller {
+  connect() {
+    const messageArea = document.querySelector("#new_messages");
+
+    if (messageArea) {
+      const observer = new MutationObserver(() => {
+        this.replaceMessageCss();
+      });
+
+      observer.observe(messageArea, { childList: true });
+    }
+  }
+
+  replaceMessageCss() {
+    const messageForm = document.querySelector("form[data-controller='messages']");
+    const userUUID = messageForm.dataset.senderUuid;
+    const newMessages = document.querySelectorAll(`.user_${userUUID}`);
+
+    newMessages.forEach((newMessage) => {
+      newMessage.classList.remove(`user_${userUUID}`);
+      newMessage.classList.add("sent-message");
+    });
+  }
+
   clear() {
     this.element.reset();
   }
